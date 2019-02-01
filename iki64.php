59df18014f013780b96296961b06d3a26e6edc4a
milio48 <31167054+milio48@users.noreply.github.com>
2019-02-01 10:08:00 +0700

    first beta version, codename rangen
    basic, acak acakan

---
+++ iki64.php
@@ -0,0 +1,54 @@
+<?php
+
+// https://www.programmingalgorithms.com/algorithm/caesar-cipher?lang=PHP
+
+function Cipher($ch, $key)
+{
+	if (!ctype_alpha($ch))
+		return $ch;
+
+	$offset = ord(ctype_upper($ch) ? 'A' : 'a');
+	return chr(fmod(((ord($ch) + $key) - $offset), 26) + $offset);
+}
+
+function Encipher($input, $key)
+{
+	$output = "";
+
+	$inputArr = str_split($input);
+	foreach ($inputArr as $ch)
+		$output .= Cipher($ch, $key);
+
+	return $output;
+}
+
+function Decipher($input, $key)
+{
+	return Encipher($input, 26 - $key);
+}
+
+// iki64
+
+function iki64_encode($data, $key){
+    $satu   = base64_encode($data);
+    $keyLen = strlen($key);
+    $dua    = Encipher($satu, $keyLen);
+    $tiga   = str_split($key);
+    $empat  = range('a', 'z');
+    $lima   = array_unique($tiga);
+    $enam   = implode($lima);
+    $tuju   = strtr($dua, $empat, $enam);
+    return $tuju;
+};
+
+function iki64_decode($data, $key){
+    $keyLen = strlen($key);
+    $empat  = range('a', 'z');
+    $tiga   = str_split($key);
+    $dua    = Decipher($data, $keyLen);
+    $lima   = array_unique($tiga);
+    $enam   = implode($lima);
+    $tuju   = strtr($dua, $enam, $eepat);
+    $satu   = base64_decode($tuju);
+    return $satu;
+};
