e2c29a3eeed25289a55e94960ea7c77cc7670c6e
milio48 <31167054+milio48@users.noreply.github.com>
2019-04-26 04:49:42 +0700

    codename : gudiken

--- example.php
+++ example.php
@@ -1,11 +1,21 @@
 <?php
 include("iki64.php");
-$mode = $_GET['mode'];
-$data = $_GET['data'];
-$key  = $_GET['key'];
+$mode = $_POST['mode'];
+$data = $_POST['data'];
+$key  = $_POST['key'];
 
-if($mode == 'enc'){
+
+echo "<html><h3>iki64</h3>
+<form method='post' autocomplete='off'>
+    input: <textarea name='data'>$_POST[data]</textarea><br>
+    key: <input type='text' name='key' value='$_POST[key]'><br>
+    <input type='submit' name='mode' value='encode'> <input type='submit' name='mode' value='decode'>
+</form><br><br>";
+
+if($mode == 'encode'){
+    echo '<br><sup>encode :</sup><hr>';
     echo iki64_encode($data, $key);
-}elseif($mode == 'dec'){
-    echo iki64_decode("$data", $key);
+}elseif($mode == 'decode'){
+    echo '<br><sup>decode :</sup><hr>';
+    echo iki64_decode($data, $key);
 };
